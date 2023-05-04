323010d93db54ca5ec2843a666317071f3d6fab1
kevaljthummar <kevaljthummar12@gmail.com>
2023-05-04 06:44:21 +0530

    Add project files.

---
+++ PerfectPay_MAUI_Demo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PerfectPay_MAUI_Demo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PerfectPay_MAUI_Demo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PerfectPay_MAUI_Demo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
