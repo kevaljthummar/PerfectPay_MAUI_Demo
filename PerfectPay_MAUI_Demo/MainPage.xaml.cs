c4a5dc474a72077f71dadb27ce6d604e7fcd32a6
kevaljthummar <kevaljthummar12@gmail.com>
2023-05-04 09:06:01 +0530

    Added UI & Added Business Logic

--- PerfectPay_MAUI_Demo/MainPage.xaml.cs
+++ PerfectPay_MAUI_Demo/MainPage.xaml.cs
@@ -2,23 +2,74 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	decimal bill;
+	int tip;
+	int noPerson = 1;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+		bill = Decimal.Parse(txtBill.Text);
+		CalclateTotal();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void CalclateTotal()
+    {
+        //Total tip
+        var totalTip = (bill * tip) / 100;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        //Tip by person
+        var tipByPerson = (totalTip / noPerson);
+        lblTipByPerson.Text = $"{tipByPerson:C}";
+
+        //Subtotal
+        var subTotal = (bill / noPerson);
+        lblSubtotal.Text = $"{subTotal:C}";
+
+        //Total by person
+        var totalByPerson = (bill + totalTip) / noPerson;
+        lblTotal.Text = $"{totalByPerson:C}";
+    }
+
+    private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip: {tip}%";
+
+        CalclateTotal();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if(sender is Button)
+        {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+        }
+    }
+
+    private void btnMinus_Clicked(object sender, EventArgs e)
+    {
+        if(noPerson > 1)
+        {
+            noPerson--;
+        }
+        lblNoPerson.Text = noPerson.ToString();
+
+        CalclateTotal();
+    }
+
+    private void btnPlus_Clicked(object sender, EventArgs e)
+    {
+        noPerson++;
+        lblNoPerson.Text = noPerson.ToString();
+
+        CalclateTotal();
+    }
 }
 
